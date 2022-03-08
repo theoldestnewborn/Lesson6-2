11df8a285422366002747db826c738b9c0270ca3
theoldestnewborn <theoldestnewborn@gmail.com>
2022-03-09 01:41:04 +0300

    Lesson6task2

--- src/com/company/Computer.java
+++ src/com/company/Computer.java
@@ -3,27 +3,32 @@ package com.company;
 public class Computer {
     int price;
     String model;
-    int hdd;
-    int ram;
+    int ram, drive;
     RAM ram1;
-    DRIVE hdd1;
+    DRIVE drive1;
 
 
     public Computer(int price, String model) {
         this.price = price;
         this.model = model;
         ram1 = new RAM("MegaRam", 1024);
-        hdd1 = new DRIVE("MegaDRIVE", 160, "SSD");
+        drive1 = new DRIVE("MegaDRIVE", 160, "SSD");
+
     }
 
-    public Computer(int price, String model, int ram, int hdd) {
+    void getInfoComputer1 () {
+        System.out.println("Price - " + price + "\nModel - " + model);
+        ram1.getInfoRam();
+    }
+
+    public Computer(int price, String model, int ram, int drive) {
         this.price = price;
         this.model = model;
         this.ram = ram;
-        this.hdd = hdd;
+        this.drive = drive;
     }
-    void getInfo () {
-        System.out.println("Price - " + price + "\nModel - " + model + "\nRAM - " + ram + "\nHDD - " + hdd);
+    void getInfoComputer2 () {
+        System.out.println("\nPrice - " + price + "\nModel - " + model + "\nRAM - " + ram + "\nHDD - " + drive);
     }
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Computer pc1 = new Computer(500,"MegaPC-lite");
+
+        pc1.getInfoComputer1();
+
+
+        Computer pc2 = new Computer (300,"MegaPC", 2048, 240);
+        pc2.getInfoComputer2();
+
+
 
 
     }
 }
+
+
+
